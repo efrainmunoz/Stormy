83c627f6e7c6019abaa422c6c88d10383ce9ef20
Efraín Muñoz Coreano <munozcoreano@gmail.com>
2016-10-24 18:08:11 -0400

    Make Asynchronous HTTP request.

--- app/src/main/java/com/efrainmunoz/stormy/MainActivity.java
+++ app/src/main/java/com/efrainmunoz/stormy/MainActivity.java
@@ -7,6 +7,7 @@ import android.util.Log;
 import java.io.IOException;
 
 import okhttp3.Call;
+import okhttp3.Callback;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
 import okhttp3.Response;
@@ -20,24 +21,39 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        // api parameters
         String apiKey = "31aba2d82602cd0abe6741c852737177";
         double lat = 37.8267;
         double lng = -122.4233;
         String forecastURL = "https://api.darksky.net/forecast/"
                 + apiKey +"/" + lat + "," + lng;
 
+        // HTTP client
         OkHttpClient client = new OkHttpClient();
+
+        // HTTP request
         Request request = new Request.Builder()
                 .url(forecastURL)
                 .build();
+
+        // Send request
         Call call = client.newCall(request);
-        try {
-            Response response = call.execute();
-            if (response.isSuccessful()) {
-                Log.v(TAG, response.body().string());
+        call.enqueue(new Callback() {
+            @Override
+            public void onFailure(Call call, IOException e) {
+
             }
-        } catch (IOException e) {
-            Log.e(TAG, "Exception caught: ", e);
-        }
+
+            @Override
+            public void onResponse(Call call, Response response) throws IOException {
+                try {
+                    if (response.isSuccessful()) {
+                        Log.v(TAG, response.body().string());
+                    }
+                } catch (IOException e) {
+                    Log.e(TAG, "Exception caught: ", e);
+                }
+            }
+        });
     }
 }
